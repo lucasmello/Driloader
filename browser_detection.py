e2b86900438db27324f9525f0bc3415bf6a70c0d
lucasmello <lucaseverain@gmail.com>
2017-05-29 23:12:08 -0300

    Fixing registry finding in Linux.

--- browser_detection.py
+++ browser_detection.py
@@ -19,10 +19,10 @@ class BrowserDetection:
             return res_reg.group(0)
 
     def get_firefox_version(self):
-        ff_path = self._find_firefox_exe_in_registry()
         if self.OS == "linux":
             output = subprocess.getoutput("firefox -v")
         elif self.OS == "win32":
+            ff_path = self._find_firefox_exe_in_registry()
             output = subprocess.getoutput('"{}" -v | more'.format(ff_path))
         if output is not None:
             out_reg = re.search(self.pattern, output)
