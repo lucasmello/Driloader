99b0f517a5e3b5dd24d9aac32334a3a8b7966e63
lucasmello <lucaseverain@gmail.com>
2017-05-29 23:03:09 -0300

    Adding firefox search on Windows and Linux.

--- browser_detection.py
+++ browser_detection.py
@@ -1,7 +1,6 @@
 import re
 import subprocess
 import sys
-import os
 
 
 class BrowserDetection:
@@ -18,4 +17,43 @@ class BrowserDetection:
             result = subprocess.getoutput(cmd)
             res_reg = re.search(self.pattern, result)
             return res_reg.group(0)
-            # wmic datafile where name="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" get Version /value
+
+    def get_firefox_version(self):
+        ff_path = self._find_firefox_exe_in_registry()
+        if self.OS == "linux":
+            output = subprocess.getoutput("firefox -v")
+        elif self.OS == "win32":
+            output = subprocess.getoutput('"{}" -v | more'.format(ff_path))
+        if output is not None:
+            out_reg = re.search(self.pattern, output)
+            return out_reg.group(0)
+
+
+    def _find_firefox_exe_in_registry(self):
+        try:
+            from _winreg import OpenKey, QueryValue, HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER
+        except ImportError:
+            from winreg import OpenKey, QueryValue, HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER
+        import shlex
+        keys = (r"SOFTWARE\Classes\FirefoxHTML\shell\open\command",
+                r"SOFTWARE\Classes\Applications\firefox.exe\shell\open\command")
+        command = ""
+        for path in keys:
+            try:
+                key = OpenKey(HKEY_LOCAL_MACHINE, path)
+                command = QueryValue(key, "")
+                break
+            except OSError:
+                try:
+                    key = OpenKey(HKEY_CURRENT_USER, path)
+                    command = QueryValue(key, "")
+                    break
+                except OSError:
+                    pass
+        else:
+            return ""
+
+        if not command:
+            return ""
+
+        return shlex.split(command)[0]
