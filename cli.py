90783b95d426f0753ca8efeac5ec3a59d9adb05f
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-06-04 17:34:19 -0300

    Improving Exception handling inc cli.py

--- cli.py
+++ cli.py
@@ -14,30 +14,35 @@ import sys
 import argparse
 from driloadder.browser_detection import BrowserDetection
 
+class CliError(Exception):
+    """ CliError """
+    def __init__(self, message, cause):
+        super(CliError, self).__init__(message)
+        self.cause = cause
 
 class DriloadderCommands():
+    """Class that accts as facade to BrowserDetection"""
+
     def __init__(self):
-        self.bd = BrowserDetection()
-        self.exit_stdout = 0
-        self.exit_stderr = 1
+        """Init method
+
+        Creates a new instance of BrowserDetection
+
+        """
+
+        self.browser_detection = BrowserDetection()
 
     def get_google_chrome_version(self):
-        return ((self.bd.get_chrome_version()), self.exit_stdout)
+        return self.browser_detection.get_chrome_version()
 
     def get_firefox_version(self):
-        return (self.bd.get_firefox_version(), self.exit_stdout)
-
+        return self.browser_detection.get_firefox_version()
 
     def get_internet_explorer_version(self):
         try:
-            return (self.bd.get_internet_explorer_version(), self.exit_stdout)
+            return self.browser_detection.get_internet_explorer_version()
         except OSError as err:
-            return (self.format_error_output('Unable to get Internet Explorer version. Cause: {}'.format(str(err))),
-                    self.exit_stderr)
-
-    @staticmethod
-    def format_error_output(error):
-        return "A error happend: {}".format(error)
+            raise CliError('Unable to get Internet Explorer version', str(err))
 
 
 def show_python_version_error_error(python_version, acceptable_major, acceptable_minor):
@@ -66,10 +71,22 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # parser.add_argument('--verbose', '-v', help='Show more messages', action='store_true')
     action = parser.add_mutually_exclusive_group(required=True)
-    action.add_argument('--firefox', '-f', help='get Firefox version.', action='store_true')
-    action.add_argument('--chrome', '-c', help='get Google Chrome version.', action='store_true')
-    action.add_argument('--internet-explorer', '-i', help='get Internet Explorer version.', action='store_true')
-    action.add_argument('--all', help='look for browsers an get their versions', action='store_true')
+    action.add_argument('--firefox', '-f',
+                        help='get Firefox version.',
+                        action='store_true')
+
+    action.add_argument('--chrome', '-c',
+                        help='get Google Chrome version.',
+                        action='store_true')
+
+    action.add_argument('--internet-explorer', '-i',
+                        help='get Internet Explorer version.',
+                        action='store_true')
+
+    action.add_argument('--all',
+                        help='look for browsers an get their versions',
+                        action='store_true')
+
     args = parser.parse_args()
 
     for key, value in args.__dict__.items():
@@ -86,17 +103,20 @@ def main():
         'internet_explorer': commands.get_internet_explorer_version
     }
 
-    result = options[option]()
-    message = result[0]
-    exit_code = result[1]
-    if exit_code == 1:
+    exit_code = 0
+    output_type = sys.stdout
+
+    try:
+        result = options[option]()
+        message = result
+
+    except CliError as cli_error:
+        message = 'Error: {}.\nCause: {}'.format(str(cli_error), cli_error.cause)
+        exit_code = 1
         output_type = sys.stderr
-    else:
-        output_type = sys.stdout
 
     print(message, file=output_type)
     sys.exit(exit_code)
-
 
 if __name__ == '__main__':
     check_python_version()
