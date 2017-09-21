7014682c462b74a11d77023b36074d28e5c19bbc
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-09-21 17:29:45 -0300

    Adds QA and Module classifiers

--- setup.py
+++ setup.py
@@ -14,9 +14,13 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'
     ]
 )
