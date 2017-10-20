2a3a10fdd693987608d5a32d09cda456a0e210b9
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-10-20 15:31:09 -0200

    Adds documentation to tests; added tests to lint

--- tests/test_class_command.py
+++ tests/test_class_command.py
@@ -1,20 +1,58 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+# Disabling the below pylint warnings in order to use long names convention to tests
+# and because pytest and pytest_mock imports are used seamlessly.
+
+# pylint: disable=invalid-name
+# pylint: disable=unused-import
+
+
+
+"""
+tests.test_class_commands
+------------------
+
+The test set for functions in driloader.commands.Commands
+"""
+
+
 import pytest
 import pytest_mock
 
 from driloader.commands import CommandError, Commands
 
 
-def test_commands_call_to_subprocess_check_output_with_a_valid_command(mocker):
-    mocker.patch('subprocess.check_output', return_value=b'cmd_output')
-    output = Commands.run("a_valid_command")
-    assert output == 'cmd_output'
+class TestCommands:
+    """ Test Commands by mocking the subprocess calls """
 
-def test_raises_exception_on_running_non_existing_command_with_check_output(mocker):
-    mocker.patch('subprocess.check_output', return_value=None)
-    with pytest.raises(CommandError):
-        Commands.run('not_existing_command')
+    @staticmethod
+    def test_call_subprocess_check_output_with_a_valid_command(mocker):
+        """Test calls like subprocess.check_ouput('valiad_command') """
 
-def test_raises_exception_on_running_non_existing_command_with_run(mocker):
-    mocker.patch('subprocess.run', return_value=None, create=True)
-    with pytest.raises(CommandError):
-        Commands.run('not_existing_command')
+        mocker.patch('subprocess.check_output', return_value=b'cmd_output')
+        output = Commands.run("a_valid_command")
+        assert output == 'cmd_output'
+
+    @staticmethod
+    def test_raises_exception_on_running_non_existing_command_with_check_output(mocker):
+        """Test calls like subprocess.check_ouput('invaliad_command')
+           where the CommandError exception should be thrown.
+        """
+
+        mocker.patch('subprocess.check_output', return_value=None)
+        with pytest.raises(CommandError):
+            Commands.run('not_existing_command')
+
+    @staticmethod
+    def test_raises_exception_on_running_non_existing_command_with_run(mocker):
+        """Test calls like subprocess.run('invaliad_command')
+           where the CommandError exception should be thrown.
+
+           For versions < 3.6, the subprocess,run function doesn't exists, so
+           the mock creates this function by using `create=True`.
+        """
+
+        mocker.patch('subprocess.run', return_value=None, create=True)
+        with pytest.raises(CommandError):
+            Commands.run('not_existing_command')
--- tests/test_class_downloader.py
+++ tests/test_class_downloader.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+# Disabling the below pylint warnings in order to use long names convention to tests
+# and because pytest and pytest_mock imports are used seamlessly.
+
+# pylint: disable=invalid-name
+# pylint: disable=unused-import
+# pylint: disable=too-few-public-methods
+
+
+
+"""
+tests.test_class_downloader
+------------------
+
+The test set for functions in driloader.downloader.Downloader
+"""
+
+
 import os
 import random
 
@@ -7,22 +27,44 @@ import pytest_mock
 from requests import Response
 
 from driloader.downloader import Downloader
-from driloader.browsers import GECKODRIVER, CHROMEDRIVER, IEDRIVER
+from driloader.browsers import IEDRIVER
 
 
-def test_download_driver_file(mocker):
-    mock_file_content = str(random.random())
-    mock_file_name = './test_driver_downloader.txt'
+class TestDownloader:
+    """ Test Downloader by mocking the requests calls """
 
-    mocker.patch.object(Response, 'content')
-    Response.content = bytes(mock_file_content, 'UTF-8')
+    @staticmethod
+    def test_download_driver_file(mocker):
+        """ Test the download of a file by mocking the
+            requests.Response object as a text file with a random number.
 
-    downloader = Downloader(GECKODRIVER)
-    downloader.download_file('http://test.com', mock_file_name)
+            The assertion consists in checking if the content saved by the tested
+            function is a text file with the same random number.
 
-    with open(mock_file_name, 'r') as file:
-        file_content = file.read()
-    file.close()
-    os.remove(mock_file_name)
+            Using IEDRIVER and mocking the needed functions from driloader.browsers
+            to ensure the mocking would take care of all real system information.
 
-    assert file_content == mock_file_content
+            This test takes usually longer than 1s, probably because of IO operations
+            run in downloader.download_file().
+        """
+
+        mocked_response = str(random.random())
+        mock_file_name = './test_driver_downloader.txt'
+
+        mocker.patch.object(Response, 'content')
+        Response.content = bytes(mocked_response, 'UTF-8')
+
+        mocker.patch('platform.system', return_value='Windows')
+        mocker.patch('driloader.browsers.Browser.__init__', return_value=None)
+        mocker.patch('driloader.downloader.Downloader._create_driver_folder',
+                     return_value='hidden_name')
+
+        downloader = Downloader(IEDRIVER)
+        downloader.download_file('http://test.com', mock_file_name)
+
+        with open(mock_file_name, 'r') as file:
+            file_content = file.read()
+        file.close()
+        os.remove(mock_file_name)
+
+        assert file_content == mocked_response
