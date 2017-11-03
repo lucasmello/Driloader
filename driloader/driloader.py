8e9232a349865256673b81e1038d7de21ff4e205
lucasmello <lucasmello@outlook.com>
2017-11-03 16:48:17 -0200

    Adding documentation to all methods.

--- driloader/driloader.py
+++ driloader/driloader.py
@@ -17,19 +17,48 @@ from .commands import Commands
 from .downloader import Downloader
 
 
-def download_chrome_driver(path_to_download="default", version="autodetect", proxy={}):
+def download_chrome_driver(path_to_download="default", version="autodetect", proxy=None):
+    """
+    Downloads the chrome driver.
+    :param path_to_download: the path to put the file in.
+    :param version: specify the chrome driver's version.
+    :param proxy: proxy Dict. e.g {'http': '1.2.3.4', 'https': '5.6.7.8'}
+    :return: the full path to the unzipped chrome driver.
+    """
     return download_driver(path_to_download, version, CHROMEDRIVER, proxy)
 
 
-def download_gecko_driver(path_to_download="default", version="autodetect", proxy={}):
+def download_gecko_driver(path_to_download="default", version="autodetect", proxy=None):
+    """
+    Downloads the gecko driver (Firefox).
+    :param path_to_download: the path to put the file in.
+    :param version: specify the gecko driver's version.
+    :param proxy: proxy Dict. e.g {'http': '1.2.3.4', 'https': '5.6.7.8'}
+    :return: the full path to the unzipped gecko driver.
+    """
     return download_driver(path_to_download, version, GECKODRIVER, proxy)
 
 
-def download_ie_driver(path_to_download="default", version="autodetect", proxy={}):
+def download_ie_driver(path_to_download="default", version="autodetect", proxy=None):
+    """
+    Downloads the Internet Explorer driver.
+    :param path_to_download: the path to put the file in.
+    :param version: specify the ie driver's version.
+    :param proxy: proxy Dict. e.g {'http': '1.2.3.4', 'https': '5.6.7.8'}
+    :return: the full path to the unzipped ie driver.
+    """
     return download_driver(path_to_download, version, IEDRIVER, proxy)
 
 
 def download_driver(path_to_download, version, browser, proxy):
+    """
+    Downloads the driver according to browser parameter.
+    :param path_to_download: the path to put the file in.
+    :param version: the driver version.
+    :param browser: the browser to download it's specific driver.
+    :param proxy: proxy Dict. e.g {'http': '1.2.3.4', 'https': '5.6.7.8'}
+    :return: the full unzipped driver's path.
+    """
     driver = Downloader(browser)
 
     if version == "autodetect":
