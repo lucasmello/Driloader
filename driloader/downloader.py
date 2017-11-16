72adabb2588fa4dc23ee15ba82456400e75975f2
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-11-16 20:49:26 -0200

    Improve Proxy Singleton class

--- driloader/downloader.py
+++ driloader/downloader.py
@@ -65,7 +65,7 @@ class Downloader:
         """
         if not os.path.exists(path_to_download):
             requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-            response = requests.get(url, verify=False, proxies=Proxy.proxy)
+            response = requests.get(url, verify=False, proxies=Proxy().url)
             path = path_to_download.rpartition("/")[0]
             if not os.path.exists(path):
                 os.makedirs(path)
--- driloader/proxy.py
+++ driloader/proxy.py
@@ -1,21 +1,55 @@
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
 # pylint: disable=too-few-public-methods
 
-
 """
-Responsible to keep proxy configurations.
-"""
+driloader.proxy
+------------------
 
+Proxy is responsible for keeping proxy configurations for the whole module.
+This singleton class must be assigned to a variable using the `url` argument only once.
+The next assignments must use no arguments and then, the previously assigned value will be used.
+
+Example of usage:
+>>> from proxy import Proxy
+>>> proxy = Proxy('http://proxy.company.com:3128')
+>>> proxy.url
+'http://proxy.company.com:3128'
+>>> proxy
+<proxy.Proxy object at 0x7fd6df1797b8>
+>>> proxy2 = Proxy()
+>>> proxy2
+<proxy.Proxy object at 0x7fd6df1797b8>
+>>> proxy2.url
+'http://proxy.company.com:3128'
+>>> Proxy().url
+'http://proxy.company.com:3128'
+"""
 
 class Proxy:
-
     """
-    Responsible to keep proxy configurations.
-    """
+    Singleton class to store the proxy configuration. It must be
+    initialized in when the main driloader module is called.
 
+    __instance: class variable to store the only class instance.
+    """
     __instance = None
 
-    def __new__(cls, proxy):
+    def __new__(cls, url=None):
+        """
+        Static function responsible for creating a new instance of Proxy.
+        This implementation aims to create a single instance of Proxy in the first
+        assignment and then only return this instance.
+
+        This current implementation doesn't allow multiple assignments to Proxy.
+        Once assigned, the instance will not take a new value, unless explicitly set by
+        using `instance_variable.url = 'new value'`
+
+        :param url: The proxy url in the format [http|https]://[HOST]:[PORT].
+        """
         if Proxy.__instance is None:
             Proxy.__instance = object.__new__(cls)
-        Proxy.__instance.proxy = proxy
+            Proxy.__instance.url = url
+
         return Proxy.__instance
